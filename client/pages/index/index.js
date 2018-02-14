5a9e89cfffab3b0018375de3dbd9ae31414dc86a
xinyuchen <1503750106@qq.com>
2018-02-14 16:02:54 +0800

    图片预览增加动画效果

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -221,6 +221,33 @@ Page({
   previewImage: function (e) {
     var status = e.currentTarget.dataset.status
     
+    //第1步：创建动画实例
+    var animation = wx.createAnimation({
+      duration: 350,
+      transformOrigin: '50% 100% 0'
+    })
+
+    //第2步：这个动画实例赋给当前动画实例
+    this.animation = animation
+
+    //第3步：执行第一组动画
+    animation.opacity(0).step();
+
+    // 第4步：导出动画对象赋给数据对象储存 
+    this.setData({
+      animationData: animation.export()
+    })
+
+    // 第5步：设置定时器到指定时候后，执行第二组动画 
+    setTimeout(function () {
+      // 执行第二组动画 
+      animation.opacity(1).step();
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象 
+      this.setData({
+        animationData: animation
+      })
+    }.bind(this), 200)
+
   if (status == 'open') {
       this.setData({
         previewImage: false,
